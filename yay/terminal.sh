f7166184ae3ebaf1f34e00e930300f8febaed511
Astrid REDACTED IV <astridivy@astrid.computer>
2019-09-11 00:39:30 -0700

    add tty clock to terminal

--- yay/terminal.sh
+++ yay/terminal.sh
@@ -1,7 +1,11 @@
 #!/bin/bash
+# run this as *not* root
+
 # red standard err
 yay stderred
 # git autocomplete
 git clone https://github.com/git/git --depth 1
 cp -i git/contrib/completion/git-completion.bash ~/.git-autocomplete.bash
 rm git -rf
+# tty-clock
+yay tty-clock
