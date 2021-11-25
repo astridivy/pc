6f1901aa3f5f516a8623e72f117f1edf4145c5e1
Astrid Ivy <git@astrid.host>
2021-11-25 00:51:13 -0700

    latest blackbox stuff 2021

---
+++ yay/blackbox.sh
@@ -0,0 +1,12 @@
+yay wmcpuload
+
+yay wmclockmon
+
+yay wmmemload
+
+# may have trouble building ; if so , temporarily replace "rm" with a version of rm that always succeeds
+yay blackboxwm
+
+yay bbkeys
+
+yay bbconf
